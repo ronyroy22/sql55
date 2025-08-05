a659e5baf527818b74c77795577ea753f30828ba
ronyroy22 <rounyroy45@gmail.com>
2025-08-05 01:57:52 -0700

    Add files via upload

--- exp 2.sql
+++ exp 2.sql
@@ -1,1 +1,25 @@
 create database student;
+create database employee;
+use student;
+create table StInfo(stId int primary key, stName varchar(20),stAge int ,stMobile varchar(10),stEmail varchar(25));
+create table StResult(stId int primary key, stSub1 int, stSub2 int, stSub3 int,stSub4 int, stSub5 int, stMarks int);
+desc stInfo;
+show tables;
+drop table StResult; /*delete table*/
+show tables;
+alter table StInfo add(stFName varchar(15));
+desc stinfo;
+alter table StInfo drop stFName;
+alter table StInfo rename to StInformation;
+insert into StInformation values (1, "HONEY",19,9636844480,"honey@poornima.edu.in");
+insert into StInformation values (2, "MOHAN",21,9012345678,"mohan@poornima.edu.in");
+insert into StInformation values (3, "KARAN",24,9678102345,"karan@poornima.edu.in");
+insert into StInformation values (4, "SOHAN",23,9321045678,"sohan@poornima.edu.in");
+insert into StInformation values (5, "DINKER",19,9467832105,"dinker@poornima.edu.in");
+select * from stinformation;
+insert into stinformation values
+(2307,"NANDINI",23,9587640123,"nandini@gmail.com"),
+(2308,"CHHAVI",22,9612308547,"chhavi@gmail.com");
+select * from stinformation;
+update stinformation set stAge=21 where StId=2307;
+delete from stinformation where stid=2308;
--- exp3.sql
+++ exp3.sql
@@ -1,5 +1,6 @@
 use student;
 select * from stinformation;
+select * from stresult;
 select stName,StEmail from stinformation;
 select * from stinformation where stAge=19;
 select stName, stMobile from stinformation where stAge > 19;
@@ -14,5 +15,20 @@ create table stFees (stID int, totFee int, stPaidFee int, stDueFee int);
 desc stResult;
 desc stfees;
 insert into stresult values (1, 98), (2, 95), (3, 93), (4, 90), (5, 93);
-select * from stredult;
-
+insert into stfees values (1, 98000, 90000, 8000),
+(2, 95000, 90000, 5000),
+(3, 93000, 90000,3000),
+(4, 90000, 85000, 5000),
+(5, 93000, 13000, 80000);
+select * from stfees;
+desc stinformation;
+select stinformation.stid, stinformation.stName, stresult.stmarks
+from stinformation, stresult
+where stinformation.stid = stresult.stid;
+/*exp 14 */
+select stinformation.stid, stinformation.stName, stresult.stmarks
+from stinformation, stresult
+where stinformation.stid = stresult.stid;
+/*exp 15 */
+select stname from stinformation union select stmarks from stresult;
+/*exp 16 */
