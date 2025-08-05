5faf4ec65268005602688d849899fba7975c19e9
ronyroy22 <rounyroy45@gmail.com>
2025-08-05 01:58:47 -0700

    Update exp 4.sql

---
+++ exp 4.sql
@@ -0,0 +1,14 @@
+use student;
+select * from stinformation;
+select * from stresult;
+select * from stfees;
+create view age as select stid, stname, stage from stinformation;
+select * from age;
+create view temp as select stinformation.stid, stinformation.stname,stinformation.stage,
+stresult.stmarks from stinformation,stresult
+where stinformation.stid = stresult.stid;
+select * from temp;
+/*exp 6*/
+alter table stresult add (SrNO int auto_increment primary key);
+select * from stinformation where stage > 20;
+select * from stinformation where stfees < 10000;
